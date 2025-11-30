376b2d4aa0b48c642776afb5cc89b20b3485ef1a
Kinthu123 <icedevil196@gmail.com>
2025-11-30 15:21:55 +0700

    chore: add project dependencies and ESLint configuration.

---
+++ my-app/app/page.tsx
@@ -0,0 +1,23 @@
+import ExploreBtn from "@/components/ExploreBtn"
+
+const page = () => {
+  return (
+    <section>
+      <h1 className="text-center mt-8">The Hub for Every Developer<br /> Event You Cannot Miss</h1>
+      <p className="text-center mt-4">Hackathon, Meetups, and Conferences, All in One Place</p>
+      <ExploreBtn />
+
+      <div className="mt-20 space-y-8">
+        <h3>Featured Events</h3>
+
+        <ul className="events">
+          {[1,2,3,4,5].map((event)=>(
+            <li key={event}>Event{event}</li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  )
+}
+
+export default page
